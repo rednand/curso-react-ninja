578b193260db6126c9b7df00820dd003327d6748
Renan Aguiar <renan.modesto@grupomultiplica.com.br>
2022-01-25 15:36:10 -0300

    alteracao do titulo

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 "use strict";
-import React from 'react';
+import React from "react";
 
 var Title = React.createClass({
-  render: function() {
-    return <h1>Titulo</h1>;
+  render: function () {
+    return <h1>Horror movies this weekend</h1>;
   },
 });
 
